2663d82f517b8a0813492c0ace35b5aedf15ffdb
Matilda Steiner <142055238+Tilly-11@users.noreply.github.com>
2025-04-04 17:11:00 +0000

    Update Bronze_DDL.sql

--- scripts/Bronze/Bronze_DDL.sql
+++ scripts/Bronze/Bronze_DDL.sql
@@ -1,13 +1,14 @@
 /*
 ===============================================================
-Script Name  : Bronze_DDL.sql
-Description  : This script defines the schema and table structure  
+Script Name: Bronze_DDL.sql
+Description: This script defines the schema and table structure  
                for the Bronze layer of the Data Warehouse.  
                It includes database creation, schema setup,  
                and table definitions.
 ===============================================================
 */
 
+DROP TABLE IF EXISTS Bronze.crm_cust_info;
 CREATE TABLE Bronze.crm_cust_info(
 cust_id INT,
 cust_key VARCHAR(50),
@@ -19,6 +20,7 @@ cst_create_date DATE
 
 );
 
+DROP TABLE IF EXISTS Bronze.crm_product_info;
 CREATE TABLE Bronze.crm_product_info(
 prd_id INT,
 prd_key VARCHAR(50),
@@ -29,6 +31,7 @@ prd_start_dt DATETIME,
 prd_end_dt DATETIME
 );
 
+DROP TABLE IF EXISTS Bronze.crm_sales_details;
 CREATE TABLE Bronze.crm_sales_details(
 sls_ord_num VARCHAR(50),
 sls_prd_key VARCHAR(50),
@@ -41,18 +44,20 @@ sls_quantity INT,
 sls_price INT
 );
 
+DROP TABLE IF EXISTS Bronze.erp_loc_a101;
 CREATE TABLE Bronze.erp_loc_a101(
 cid VARCHAR(50),
 cntry VARCHAR(50)
 );
 
-
+DROP TABLE IF EXISTS Bronze.erp_cust_az12;
 CREATE TABLE Bronze.erp_cust_az12(
 cid VARCHAR(50),
 bdate DATE,
 gen VARCHAR(50)
 );
 
+DROP TABLE IF EXISTS Bronze.erp_px_cat_g1v2;
 CREATE TABLE Bronze.erp_px_cat_g1v2(
 id VARCHAR(50),
 cat VARCHAR(50),
